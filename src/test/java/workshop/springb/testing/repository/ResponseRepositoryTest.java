08793c3437f309279c07ba38713dc2e63e7c519d
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2022-01-17 22:54:28 +0100

    todo done

--- src/test/java/workshop/springb/testing/repository/ResponseRepositoryTest.java
+++ src/test/java/workshop/springb/testing/repository/ResponseRepositoryTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
 import workshop.springb.testing.model.Response;
 
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.annotation.DirtiesContext.*;
 
 /*
     TODO 2 README
@@ -20,6 +22,7 @@ import static org.junit.jupiter.api.Assertions.*;
     Każdy test będzie uruchomiony w springowej transakcji.
  */
 @DataJpaTest
+@DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
 class ResponseRepositoryTest {
 
     /*
@@ -45,9 +48,7 @@ class ResponseRepositoryTest {
         );
     }
 
-}
-
-/*
+    /*
  _______________________________________________________________________________________________________________________
  TODO 1 README
 
@@ -61,12 +62,55 @@ class ResponseRepositoryTest {
  Dla tego zadania nie ma gotowego rozwiązania - na tym etapie, nie powinno stanowić dla Ciebie problemu nie do roziwązania :)
 
  _______________________________________________________________________________________________________________________
-                                                         \
-                                                          \
-                                                            /  \~~~/  \
-                                                           (    ..     )~~~~,
-                                                            \__     __/      \
-                                                              )|  /)         |\
-                                                               | /\  /___\   / ^
-                                                                "-|__|   |__|
+
  */
+
+    @Test
+    @DisplayName("Should delete one")
+    void shouldDeleteResponse() {
+
+        assertTrue(responseRepository.findAll().isEmpty());
+        Response response = responseRepository.save(new Response("Hello, world!", LocalDateTime.now()));
+
+        responseRepository.delete(response);
+        assertTrue(responseRepository.findAll().isEmpty());
+        //fail();
+
+    }
+
+    @Test
+    @DisplayName("Should update response greeting")
+    void shouldUpdateResponseGreeting() {
+        assertTrue(responseRepository.findAll().isEmpty());
+        Response response = responseRepository.save(new Response("Hello, world!", LocalDateTime.now()));
+        response.setGreeting("Hello, Poland!");
+
+        Response upadatedResponse = responseRepository.save(response);
+
+        assertAll(
+                () -> assertEquals(1, responseRepository.findAll().size()),
+                () -> assertEquals("Hello, Poland!", responseRepository.findById(response.getId()).orElse(new Response("", LocalDateTime.now())).getGreeting())
+        );
+        //fail();
+    }
+
+    @Test
+    @DisplayName("Should findAll")
+    void shouldFindAll(){
+        assertTrue(responseRepository.findAll().isEmpty());
+        Response response1 = responseRepository.save(new Response("1", LocalDateTime.now()));
+        Response response2 = responseRepository.save(new Response("2",LocalDateTime.now()));
+        Response response3 = responseRepository.save(new Response("3",LocalDateTime.now()));
+
+        assertAll(
+                ()->assertEquals(3,responseRepository.findAll().size()),
+                ()->assertEquals("1",responseRepository.findById(response1.getId()).orElse(new Response()).getGreeting()),
+                ()->assertEquals("2",responseRepository.findById(response2.getId()).orElse(new Response()).getGreeting()),
+                ()->assertEquals("3",responseRepository.findById(response3.getId()).orElse(new Response()).getGreeting())
+
+        );
+//fail();
+    }
+
+}
+
